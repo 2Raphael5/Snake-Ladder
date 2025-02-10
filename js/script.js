4969bb4279b563093dc83e289f0d6e1845e1bbbc
mathox2 <153594364+mathox2@users.noreply.github.com>
2025-02-10 15:58:03 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-document.querySelector("#submit").addEventListener("click",submit);
+// document.querySelector("#submit").addEventListener("click",submit);
 document.querySelectorAll(".radioButton").forEach() .addEventListener("checked",changeChecked);
 
-function submit(event){
-    const form = event.target;
-    const data = new FormData(form);
+const players = document.querySelectorAll(".player");
 
-    const nbPlayer = parseInt(data.get("nbPlayer"));
-    console.log(nbPlayer);
-    const   
+// function submit(event){
+//     const form = event.target;
+//     const data = new FormData(form);
 
-    event.preventDefault();
+//     event.preventDefault();
+// }
+
+function changeChecked(event){
+    const radio = event.target;
+    for (let index = 0; index < players.length; index++) {
+        if(index < radio.value){
+            players[index].classList.add("invisible")
+            players[index].classList.remove("visible")
+        }
+        else{
+            players[index].classList.remove("invisible")
+            players[index].classList.add("visible")
+        }
+    }
 }
