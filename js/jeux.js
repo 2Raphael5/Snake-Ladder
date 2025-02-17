b0f2614b3dec9f9a9d961d4a8f49f79f18d4ad36
matthias-fsslr <matthias.fsslr@eduge.ch>
2025-02-17 15:55:52 +0100

    nouveau dossier js (ajout jeux.js + roll dice)

---
+++ js/jeux.js
@@ -0,0 +1,6 @@
+document.querySelector("#lancerDice").addEventListener("click",rollDice);
+
+function rollDice(){
+    let nombre = Math.trunc(Math.random() * 6) + 1;
+    document.querySelector("#dice").src = "img/De/dé"+nombre+".png";
+}
