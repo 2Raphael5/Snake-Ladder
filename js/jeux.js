0407b5c91e3c5bdffc0122540a86cd7300c3cb50
2Raphael5 <151612958+2Raphael5@users.noreply.github.com>
2025-03-03 14:39:59 +0100

    Question HTML

--- js/jeux.js
+++ js/jeux.js
@@ -1,5 +1,4 @@
-document.querySelector("#lancerDice").addEventListener("click",rollDice);
-
+document.querySelector("#lancerDice").addEventListener("click",rollDice); 
 function rollDice(){
     let nombre = Math.trunc(Math.random() * 6) + 1;
     document.querySelector("#dice").src = "img/De/dé"+nombre+".png";
